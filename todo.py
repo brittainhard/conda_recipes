12bc0461d372124558d52af758675f4a9953360e
Brittain Hard <brittainhard@gmail.com>
2017-09-06 18:29:13 -0500

    Improvements.

--- todo.py
+++ todo.py
@@ -1,4 +1,5 @@
 import subprocess
+import shlex
 import pandas as pd
 
 
@@ -25,11 +26,12 @@ def get_package_list(package_list, condition=32):
     elif condition == 2:
         py3 = False
         py2 = True
-    elif condition is 3:
+    elif condition == 3:
         py3 = True
         py2 = False
     else:
         assert 0
+
     packages = []
     for package_name in package_list:
         packages.append({
@@ -38,18 +40,53 @@ def get_package_list(package_list, condition=32):
             "available": False,
             "python3": py3,
             "python2": py2,
+            "build": ""
         })
     return packages
 
 
 def search_packages(package_list):
-    for x in package_list:
-        search_string = "conda search --platform linux-64 {}={} --spec {}".format(x["name"], y["version"], CHANNELS)
+    for package in package_list:
+        search_string = "conda search --platform linux-64 {}={} --spec {}".format(package["name"], package["version"], CHANNELS)
         result = process_call(search_string)
+        if not result[1]:
+            py3 = "py3" in str(result[0])
+            py2 = "py27" in str(result[0])
+            if package["python2"] and package["python3"]:
+                if not (py2 or py3):
+                    package["available"] = True
+                elif py2 and not py3:
+                    package["available"] = False
+                    package["build"] = "py3"
+                elif py3 and not py2:
+                    package["available"] = False
+                    package["build"] = "py2"
+                elif py2 and py3:
+                    package["available"] = True
+
+            if package["python2"] and not py2:
+                package["available"] = False
+                package["build"] = "py2"
+            else:
+                package["available"] = True
+
+            if package["python3"] and not py3:
+                package["available"] = False
+                package["build"] = "py3"
+            else:
+                package["available"] = True
+        else:
+            package["available"] = False
+            if package["python2"] and package["python3"]:
+                package["build"] = "py2py3"
+            if package["python2"] and not package["python3"]:
+                package["build"] = "py2"
+            if package["python3"] and not package["python2"]:
+                package["build"] = "py3"
+    return package_list
 
 
 def process_call(command):
-    subprocess.Popen()
     call = subprocess.Popen(shlex.split(command), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     result = call.communicate()
     return (result[0], call.returncode)
@@ -58,4 +95,5 @@ x = get_package_list(common)
 y = get_package_list(py3p, condition=3)
 z = get_package_list(py2p, condition=2)
 a = x + y + z
-# search_packages()
+df = pd.DataFrame.from_dict(search_packages(a[:10]))
+print(df)
