44f5055edb045b4858906e6c9c8390a4d8dcb9db
Brittain Hard <brittainhard@gmail.com>
2017-09-06 16:38:09 -0500

    Finishing todo.py

--- todo.py
+++ todo.py
@@ -2,6 +2,9 @@ import subprocess
 import pandas as pd
 
 
+CHANNELS = "-c glaxosmithkline -c defaults -c r"
+
+
 with open("py2k.txt", "r") as f:
     py2 = f.read()
 
@@ -15,38 +18,44 @@ py2p = list(set(py2_packages) - set(py3_packages))
 py3p = list(set(py3_packages) - set(py2_packages))
 
 
-def get_package_strings(package_list, condition=32):
-    package_dict = []
+def get_package_list(package_list, condition=32):
+    if condition == 32:
+        py3 = True
+        py2 = True
+    elif condition == 2:
+        py3 = False
+        py2 = True
+    elif condition is 3:
+        py3 = True
+        py2 = False
+    else:
+        assert 0
+    packages = []
     for package_name in package_list:
-        package_dict.append({
+        packages.append({
             "name": package_name.split("==")[0].lower(),
             "version": package_name.split("==")[-1],
-            "condition": condition,
-            "available": False
+            "available": False,
+            "python3": py3,
+            "python2": py2,
         })
-    return package_dict
-
-# versions = {
-#     "name":["r-" + x[0].lower() for x in package_strings],
-#     "version":[x[1] for x in package_strings],
-#     "available":[]
-# }
-
-def search_packages():
-    for x, y in zip(versions["name"], versions["version"]):
-        search_string = "conda search {}={} --spec".format(x, y)
-        is_available = process_call(search_string)
-        if is_available == 1:
-            versions["available"].append(False)
-        elif is_available == 0:
-            versions["available"].append(True)
+    return packages
 
 
-def process_call(search_string):
-    return subprocess.call(shlex.split(search_string))
+def search_packages(package_list):
+    for x in package_list:
+        search_string = "conda search --platform linux-64 {}={} --spec {}".format(x["name"], y["version"], CHANNELS)
+        result = process_call(search_string)
 
-x = get_package_strings(common)
-y = get_package_strings(py3p, condition=3)
-z = get_package_strings(py2p, condition=2)
+
+def process_call(command):
+    subprocess.Popen()
+    call = subprocess.Popen(shlex.split(command), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    result = call.communicate()
+    return (result[0], call.returncode)
+
+x = get_package_list(common)
+y = get_package_list(py3p, condition=3)
+z = get_package_list(py2p, condition=2)
 a = x + y + z
 # search_packages()
